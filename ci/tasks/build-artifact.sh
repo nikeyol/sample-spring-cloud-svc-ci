0d0a415fd7b01d104218116bdcbba29a5ec2b808
Mark Alston <malston@pivotal.io>
2016-08-16 17:50:51 -0700

    Polish pipeline

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -5,5 +5,5 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd sample-spring-cloud-svc-repo
 echo "Version: $version"
-./gradlew --full-stacktrace --parallel --no-daemon assemble -Pversion=$version 
-mv build/libs/sample-spring-cloud-svc-ci-*.jar ../build/
+./gradlew --full-stacktrace --parallel --no-daemon assemble -Pversion=$version
+mv build/libs/sample-spring-cloud-svc-ci-*.jar ../build/sample-spring-cloud-svc-ci.jar
