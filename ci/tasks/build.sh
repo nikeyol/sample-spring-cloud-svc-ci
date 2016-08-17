30fd56aa0d86bc889f6e23790d3eda4e634418d3
Mark Alston <malston@pivotal.io>
2016-08-16 23:25:49 -0700

    Fix mv command

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -4,4 +4,4 @@ set -e
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd repo
 ./gradlew --full-stacktrace --parallel --no-daemon assemble
-mv build/libs/sample-spring-cloud-svc-ci-*.jar ../build/sample-spring-cloud-svc-ci.jar
+mv build/libs/sample-spring-cloud-svc-ci.jar ../build/sample-spring-cloud-svc-ci.jar
