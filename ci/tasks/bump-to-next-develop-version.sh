454760971db5f437c19f18e4985f4fc046b233e8
Mark Alston <malston@pivotal.io>
2016-08-19 13:41:10 -0600

    Add better version support

--- ci/tasks/bump-to-next-develop-version.sh
+++ ci/tasks/bump-to-next-develop-version.sh
@@ -9,8 +9,14 @@ mv -f ../repo/* ./
 git remote add -f master ../repo-master
 git merge --no-edit master/master
 echo "Bump to $VERSION"
+
+rm gradle.properties
+touch gradle.properties
+cat version=$VERSION > gradle.properties
 ./mvnw versions:set -DnewVersion=${VERSION} -DallowSnapshots -Dmaven.repo.local=../m2/rootfs/opt/m2
+
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git add pom.xml
+git add gradle.properties
 git commit -m "${MESSAGE}"
--- ci/tasks/bump-to-release-version.sh
+++ ci/tasks/bump-to-release-version.sh
@@ -6,9 +6,15 @@ MESSAGE="[Concourse CI] Release $VERSION"
 cd out
 shopt -s dotglob
 mv -f ../repo/* ./
+
 echo "Bump to ($VERSION)"
+rm gradle.properties
+touch gradle.properties
+cat version=$VERSION > gradle.properties
 ./mvnw versions:set -DnewVersion=${VERSION} -Dmaven.repo.local=../m2/rootfs/opt/m2
+
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git add pom.xml
+git add gradle.properties
 git commit -m "${MESSAGE}"
