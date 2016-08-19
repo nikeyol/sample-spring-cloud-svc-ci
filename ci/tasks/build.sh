dcb8842dd7a2319745db4820e7cbfacef6bdbc93
Mark Alston <malston@pivotal.io>
2016-08-19 16:33:03 -0600

    Corrections to build.sh

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,10 +2,10 @@
 set -ex
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
-export M2_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}/../.." )" && pwd )"
+export M2_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
-mkdir -p ${M2_HOME}/conf
-cp repo/settings.xml ${M2_HOME}/conf
+mkdir $M2_HOME/conf
+cp repo/settings.xml $M2_HOME/conf
 
 cd repo
 gradle --full-stacktrace --parallel --no-daemon assemble
