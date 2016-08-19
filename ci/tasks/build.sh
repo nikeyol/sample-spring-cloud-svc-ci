ab3b1e3029f633ba10ee8e1e84f5b73729fe705c
Mark Alston <malston@pivotal.io>
2016-08-19 15:54:15 -0600

    Copy settings into m2_home/conf

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,7 +2,10 @@
 set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
-export M2_HOME=../m2/rootfs/opt/m2
+export M2_HOME=repo
+
+mkdir -p ${M2_HOME}/conf
+cp repo/settings.xml ${M2_HOME}/conf
 
 cd repo
 gradle --full-stacktrace --parallel --no-daemon assemble
