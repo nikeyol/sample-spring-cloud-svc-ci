d19da1fbc7e5de85f0871f03348b49d5abf99a37
Mark Alston <malston@pivotal.io>
2016-08-20 18:16:51 -0600

    Use gradle cache in script

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -2,7 +2,15 @@
 set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
-export M2_HOME=../m2/rootfs/opt/m2
+export M2_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+
+mkdir $M2_HOME/conf
+cp repo/settings.xml $M2_HOME/conf
+
+export GRADLE_USER_HOME=$M2_HOME
+echo "Gradle Home: ${GRADLE_USER_HOME}"
+mkdir -p $GRADLE_USER_HOME/caches/modules-2/files-2.1
+cp -R $M2_HOME/../m2/rootfs/opt/m2/. $GRADLE_USER_HOME/caches/modules-2/files-2.1/
 
 cd repo
 gradle --full-stacktrace --parallel --no-daemon test
