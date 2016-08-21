d4c149e6d129b40fbc8f7206968a576f44b785ec
Mark Alston <malston@pivotal.io>
2016-08-20 18:01:38 -0600

    Set GRADLE_USER_HOME

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -7,6 +7,11 @@ export M2_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 mkdir $M2_HOME/conf
 cp repo/settings.xml $M2_HOME/conf
 
+export GRADLE_USER_HOME=$M2_HOME
+echo "Gradle Home: ${GRADLE_USER_HOME}"
+mkdir -p $GRADLE_USER_HOME/caches/modules-2/files-2.1
+cp -R $M2_HOME/../m2/rootfs/opt/m2/ $GRADLE_USER_HOME/modules-2/files-2.1/
+
 cd repo
 gradle --full-stacktrace --parallel --no-daemon assemble
 mv build/libs/sample-spring-cloud-svc-ci.jar ../build/sample-spring-cloud-svc-ci.jar
