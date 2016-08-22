3d6fc9a112fd1c93ef5877db55f84db421dbb23d
Mark Alston <malston@pivotal.io>
2016-08-22 06:37:39 -0600

    Set REPO_HOME var

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -2,15 +2,16 @@
 set -ex
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
-export M2_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+REPO_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+M2_HOME=${REPO_HOME}/../m2/rootfs/opt/m2/
 
-# mkdir $M2_HOME/conf
-# cp repo/settings.xml $M2_HOME/conf
+# mkdir $REPO_HOME/conf
+# cp repo/settings.xml $REPO_HOME/conf
 
-export GRADLE_USER_HOME=$M2_HOME/.gradle
+export GRADLE_USER_HOME=$REPO_HOME/.gradle
 echo "Gradle Home: ${GRADLE_USER_HOME}"
 mkdir -p $GRADLE_USER_HOME/caches/modules-2/files-2.1
-cp -R $M2_HOME/../m2/rootfs/opt/m2/. $GRADLE_USER_HOME/caches/modules-2/files-2.1/
+cp -R $M2_HOME/. $GRADLE_USER_HOME/caches/modules-2/files-2.1/
 
 cd repo
 ./gradlew --full-stacktrace --parallel --no-daemon test --project-cache-dir ${GRADLE_USER_HOME}
