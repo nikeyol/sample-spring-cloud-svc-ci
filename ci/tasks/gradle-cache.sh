9f2a3e7a9508db8aba06bdc2a81fb1a5efa1bf42
Mark Alston <malston@pivotal.io>
2016-08-24 23:20:36 -0400

    Use GRADLE_USER_HOME to save cache

--- ci/tasks/gradle-cache.sh
+++ ci/tasks/gradle-cache.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+REPO_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+export GRADLE_USER_HOME=$REPO_HOME/../gradle
+echo "Gradle Home: ${GRADLE_USER_HOME}"
+
 set -ex
 
 pwd
 if [ "$1" == "init" ]; then
-	mkdir -p ../gradle/rootfs/opt/gradle
+	mkdir -p $GRADLE_USER_HOME
 fi
 
-ls -al ../gradle/rootfs/opt/gradle
+ls -al $GRADLE_USER_HOME
 
 cd repo
-./gradlew clean build --project-cache-dir ../../gradle/rootfs/opt/gradle
-cd ../../gradle
+./gradlew clean build --project-cache-dir $GRADLE_USER_HOME
+cd $GRADLE_USER_HOME
 tar -C rootfs -cf rootfs.tar .
 mv rootfs.tar ../to-push
