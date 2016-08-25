f82c695d24f7d27056322981a0ce1f35d3c0078c
Mark Alston <malston@pivotal.io>
2016-08-25 00:39:25 -0400

    Remove ref to m2 home

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,16 +2,10 @@
 set -ex
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
-export M2_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
-
-mkdir $M2_HOME/conf
-cp repo/settings.xml $M2_HOME/conf
-
-export GRADLE_USER_HOME=$M2_HOME/.gradle
+export REPO_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+export GRADLE_USER_HOME=$REPO_HOME/../gradle
 echo "Gradle Home: ${GRADLE_USER_HOME}"
-mkdir -p $GRADLE_USER_HOME/caches/modules-2/files-2.1
-cp -R $M2_HOME/../m2/rootfs/opt/m2/. $GRADLE_USER_HOME/caches/modules-2/files-2.1/
 
 cd repo
-gradle --full-stacktrace --parallel --no-daemon assemble
+./gradlew --full-stacktrace --parallel --no-daemon assemble
 mv build/libs/sample-spring-cloud-svc-ci.jar ../build/sample-spring-cloud-svc-ci.jar
--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -2,9 +2,11 @@
 set -ex
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
-REPO_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+export REPO_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 export GRADLE_USER_HOME=$REPO_HOME/../gradle
 echo "Gradle Home: ${GRADLE_USER_HOME}"
+
+ls -al $GRADLE_USER_HOME
 
 cd repo
 ./gradlew --full-stacktrace --parallel --no-daemon test --project-cache-dir ${GRADLE_USER_HOME}
