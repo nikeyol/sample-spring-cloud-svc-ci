30c618cbdccb498c00304df6783145621177ccef
Mark Alston <malston@pivotal.io>
2016-08-27 16:55:53 -0600

    Don't have to mv dirs

--- ci/tasks/scripts/upload-archives.sh
+++ ci/tasks/scripts/upload-archives.sh
@@ -5,12 +5,7 @@ VERSION=`cat version/number`
 export GRADLE_OPTS=-Dorg.gradle.native=false
 export GRADLE_USER_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../../gradle/rootfs" && pwd )"
 echo "GRADLE_USER_HOME: ${GRADLE_USER_HOME}"
-mkdir -p ${GRADLE_USER_HOME}
-ls -al $GRADLE_USER_HOME
-mv $GRADLE_USER_HOME/2.13/ $GRADLE_USER_HOME/
-mv $GRADLE_USER_HOME/caches/ $GRADLE_USER_HOME/
-mv $GRADLE_USER_HOME/wrapper/ $GRADLE_USER_HOME/
+ls -al ${GRADLE_USER_HOME}
 
-#ls -al $GRADLE_USER_HOME
 cd repo
 gradle --full-stacktrace --parallel --no-daemon upload -Pversion=${VERSION}
