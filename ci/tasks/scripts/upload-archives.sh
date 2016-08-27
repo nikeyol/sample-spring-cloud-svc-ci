9f054b545f17ddb175f7d320ab7c32f540c5ea4e
Mark Alston <malston@pivotal.io>
2016-08-27 11:30:11 -0600

    Publish artifacts to nexus after release

--- ci/tasks/scripts/upload-archives.sh
+++ ci/tasks/scripts/upload-archives.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -ex
 
+VERSION=`cat version/number`
 export GRADLE_OPTS=-Dorg.gradle.native=false
-export GRADLE_USER_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../../gradle" && pwd )"
+export GRADLE_USER_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../../gradle/rootfs" && pwd )"
 echo "GRADLE_USER_HOME: ${GRADLE_USER_HOME}"
+mkdir -p ${GRADLE_USER_HOME}
 ls -al $GRADLE_USER_HOME
-mv $GRADLE_USER_HOME/rootfs/2.13/ $GRADLE_USER_HOME/
-mv $GRADLE_USER_HOME/rootfs/caches/ $GRADLE_USER_HOME/
-mv $GRADLE_USER_HOME/rootfs/wrapper/ $GRADLE_USER_HOME/
+mv $GRADLE_USER_HOME/2.13/ $GRADLE_USER_HOME/
+mv $GRADLE_USER_HOME/caches/ $GRADLE_USER_HOME/
+mv $GRADLE_USER_HOME/wrapper/ $GRADLE_USER_HOME/
 
-ls -al $GRADLE_USER_HOME
-
+#ls -al $GRADLE_USER_HOME
 cd repo
-gradle --full-stacktrace --parallel --no-daemon uploadArchives -Dnexus.host.url=$NEXUS_URL
+gradle --full-stacktrace --parallel --no-daemon upload -Pversion=${VERSION}
