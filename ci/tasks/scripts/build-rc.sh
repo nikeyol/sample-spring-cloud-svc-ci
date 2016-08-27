3b7534cb0d1450697e0a8fe3a2e2d9e6f4bd8956
Mark Alston <malston@pivotal.io>
2016-08-27 17:44:35 -0600

    Cleanup path to gradle cache dir

--- ci/tasks/scripts/build-rc.sh
+++ ci/tasks/scripts/build-rc.sh
@@ -4,14 +4,9 @@ set -ex
 VERSION=`cat version/number`
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
-export GRADLE_USER_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../../gradle" && pwd )"
+export GRADLE_USER_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../../gradle/rootfs" && pwd )"
 echo "GRADLE_USER_HOME: ${GRADLE_USER_HOME}"
-ls -al $GRADLE_USER_HOME
-mv $GRADLE_USER_HOME/rootfs/2.13/ $GRADLE_USER_HOME/
-mv $GRADLE_USER_HOME/rootfs/caches/ $GRADLE_USER_HOME/
-mv $GRADLE_USER_HOME/rootfs/wrapper/ $GRADLE_USER_HOME/
-
-ls -al $GRADLE_USER_HOME
+ls -al ${GRADLE_USER_HOME}
 
 cd repo
 gradle --full-stacktrace --parallel --no-daemon assemble -Pversion=${VERSION}
--- ci/tasks/scripts/sonar.sh
+++ ci/tasks/scripts/sonar.sh
@@ -2,14 +2,9 @@
 set -ex
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
-export GRADLE_USER_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../../gradle" && pwd )"
+export GRADLE_USER_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../../gradle/rootfs" && pwd )"
 echo "GRADLE_USER_HOME: ${GRADLE_USER_HOME}"
-ls -al $GRADLE_USER_HOME
-mv $GRADLE_USER_HOME/rootfs/2.13/ $GRADLE_USER_HOME/
-mv $GRADLE_USER_HOME/rootfs/caches/ $GRADLE_USER_HOME/
-mv $GRADLE_USER_HOME/rootfs/wrapper/ $GRADLE_USER_HOME/
-
-ls -al $GRADLE_USER_HOME
+ls -al ${GRADLE_USER_HOME}
 
 cd repo
 gradle --full-stacktrace --parallel --no-daemon build sonarqube -Dsonar.host.url=$SONAR_URL -Dsonar.verbose=true
