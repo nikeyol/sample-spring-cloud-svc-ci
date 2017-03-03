72b7e6971b5f7daa3b85c928a4eff3ed703c5f62
Mark Alston <malston@pivotal.io>
2017-03-03 16:04:34 -0700

    Use spring-cloud-pipelines for concourse

--- concourse/start.sh
+++ concourse/start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-export ROOT_ADDRESS=${1:-`./whats_my_ip.sh`}
+ROOT_ADDRESS=${1:-`./whats_my_ip.sh`}
 
 echo "Provided external address is [${ROOT_ADDRESS}]"
 
-export CONCOURSE_LOGIN=${2:-"concourse"}
-export CONCOURSE_PASSWORD=${3:-"changeme"}
 export CONCOURSE_EXTERNAL_URL=http://${ROOT_ADDRESS}:8080
 docker-compose up -d
