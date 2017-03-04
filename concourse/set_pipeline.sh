3b351382e0a675a515537360b3ad4bd168d41f65
Mark Alston <malston@pivotal.io>
2017-03-03 22:44:03 -0700

    Fixes to cache-m2 pipeline

--- concourse/set_pipeline.sh
+++ concourse/set_pipeline.sh
@@ -3,5 +3,6 @@
 PIPELINE_NAME=${1:-sample-spring-cloud-svc}
 ALIAS=${2:-docker}
 CREDENTIALS=${3:-credentials.yml}
+PIPELINE_CONFIG=${4:-pipeline.yml}
 
-echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "${CREDENTIALS}"
+echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c "${PIPELINE_CONFIG}" -l "${CREDENTIALS}"
