1d71fe281be827e3a4470d363486da88b796358a
Mark Alston <malston@pivotal.io>
2017-03-06 15:58:07 -0600

    Use sample-spring-cloud-svc in script

--- tools/pcfdev-helper.sh
+++ tools/pcfdev-helper.sh
@@ -21,61 +21,34 @@ case $1 in
 		pcfdev_login
 
 		cf target -o pcfdev-org -s pcfdev-test
-		yes | cf stop github-webhook
-		yes | cf stop github-analytics
-		yes | cf stop github-eureka
-		yes | cf stop stubrunner
-		yes | cf stop stubrunner-github-webhook
-		yes | cf stop stubrunner-github-analytics
+		yes | cf stop sample-spring-cloud-svc
 
 		cf target -o pcfdev-org -s pcfdev-stage
-		yes | cf stop github-webhook
-		yes | cf stop github-analytics
-		yes | cf stop github-eureka
+		yes | cf stop sample-spring-cloud-svc
 
 		cf target -o pcfdev-org -s pcfdev-prod
-		yes | cf stop github-webhook
-		yes | cf stop github-webhook-venerable
-		yes | cf stop github-analytics
-		yes | cf stop github-analytics-venerable
-		yes | cf stop github-eureka
+		yes | cf stop sample-spring-cloud-svc
+		yes | cf stop sample-spring-cloud-svc-venerable
 		;;
 
 	delete-all-apps)
 		pcfdev_login
 
 		cf target -o pcfdev-org -s pcfdev-test
-		cf delete -f github-webhook
-		cf delete -f github-analytics
-		cf delete -f github-eureka
-		cf delete -f stubrunner
+		cf delete -f sample-spring-cloud-svc
 
 		cf target -o pcfdev-org -s pcfdev-stage
-		cf delete -f github-webhook
-		cf delete -f github-analytics
-		cf delete -f github-eureka
+		cf delete -f sample-spring-cloud-svc
 
 		cf target -o pcfdev-org -s pcfdev-prod
-		cf delete -f github-webhook
-		cf delete -f github-webhook-venerable
-		cf delete -f github-analytics
-		cf delete -f github-analytics-venerable
-		cf delete -f github-eureka
+		cf delete -f sample-spring-cloud-svc
+		cf delete -f sample-spring-cloud-svc-venerable
 		;;
 
 	delete-routes)
-		cf delete-route -f local.pcfdev.io -n github-webhook-test
-		cf delete-route -f local.pcfdev.io -n github-analytics-test
-		cf delete-route -f local.pcfdev.io -n github-eureka-test
-		cf delete-route -f local.pcfdev.io -n stubrunner-test
-		cf delete-route -f local.pcfdev.io -n stubrunner-github-webhook-test
-		cf delete-route -f local.pcfdev.io -n stubrunner-github-analytics-test
-		cf delete-route -f local.pcfdev.io -n github-webhook-stage
-		cf delete-route -f local.pcfdev.io -n github-analytics-stage
-		cf delete-route -f local.pcfdev.io -n github-eureka-stage
-		cf delete-route -f local.pcfdev.io -n github-analytics
-		cf delete-route -f local.pcfdev.io -n github-webhook
-		cf delete-route -f local.pcfdev.io -n github-eureka
+		cf delete-route -f local.pcfdev.io -n sample-spring-cloud-svc-test
+		cf delete-route -f local.pcfdev.io -n sample-spring-cloud-svc-stage
+		cf delete-route -f local.pcfdev.io -n sample-spring-cloud-svc
 		;;
 
 	setup-spaces)
