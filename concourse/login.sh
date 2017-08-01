7935b880cdb1f73701c5a066f2c48d8a2d05ecfe
Mark Alston <malston@pivotal.io>
2017-08-01 17:32:01 -0600

    Make login.sh creds configurable

--- concourse/login.sh
+++ concourse/login.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-ROOT_ADDRESS=${1:-localhost}
+ROOT_ADDRESS=${1:-`./whats_my_ip.sh`}
+CONCOURSE_LOGIN=${2:-"concourse"}
+CONCOURSE_PASSWORD=${3:-"changeme"}
+CONCOURSE_EXTERNAL_URL=http://${ROOT_ADDRESS}:8080
 
-fly -t docker login -c http://${ROOT_ADDRESS}:8080 -u=concourse -p=changeme
+fly -t docker login -c $CONCOURSE_EXTERNAL_URL -u=$CONCOURSE_LOGIN -p=$CONCOURSE_PASSWORD
